9e4504b6d2fc479443c5f5a42c420ddd5df50b58
Tarun1026 <tarun08531234@gmail.com>
2024-06-22 11:52:26 +0530

    Path correct

--- src/Update/edit.jsx
+++ src/Update/edit.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
+import "../HomePage/Home.css";
 
 const Edit = () => {
   const [id, setId] = useState(0);
